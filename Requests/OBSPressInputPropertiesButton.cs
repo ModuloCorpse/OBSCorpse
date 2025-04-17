7356fc37bbb0fa48523f6358b7b33fd260534d24
ModuloCorpse <mewen.lereste@wanadoo.fr>
2025-04-17 14:46:57 +0200

    [ADD] Add of PressInputPropertiesButton request

---
+++ Requests/OBSPressInputPropertiesButton.cs
@@ -0,0 +1,23 @@
+﻿using CorpseLib.DataNotation;
+
+namespace OBSCorpse.Requests
+{
+    public class OBSPressInputPropertiesButton(string inputName, OBSPressInputPropertiesButton.Properties properties) : AOBSRequest("PressInputPropertiesButton", new DataObject() { { "inputName", inputName }, { "propertyName", GetPropertiesName(properties) } })
+    {
+        public enum Properties
+        {
+            RefreshNoCache = 0
+        }
+
+        private bool m_Success = false;
+        public bool Success => m_Success;
+
+        private static string GetPropertiesName(Properties properties) => properties switch
+        {
+            Properties.RefreshNoCache => "refreshnocache",
+            _ => throw new ArgumentOutOfRangeException(nameof(properties), properties, null)
+        };
+
+        protected override void OnResponse(Response response) => m_Success = response.Result;
+    }
+}
